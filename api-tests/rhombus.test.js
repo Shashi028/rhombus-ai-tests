af94a004c197ba76d5faa997eb385ddc3fffbf1b
Shashi Singh <singh.shashi2125@gmail.com>
2026-04-20 20:01:03 +0930

    API tests complete

--- api-tests/rhombus.test.js
+++ api-tests/rhombus.test.js
@@ -2,8 +2,11 @@ import dotenv from 'dotenv';
 import { resolve } from 'path';
 import {chromium} from 'playwright';
 import axios from 'axios';
+import FormData from 'form-data';
+import fs from 'fs';
 let sessionCookie = null;
 let userEmail = null;
+let accessToken = null;
 beforeAll(async () => {
     dotenv.config({ path: resolve('..', '.env') });
     const browser = await chromium.launch();
@@ -19,6 +22,10 @@ beforeAll(async () => {
     const cookies = await context.cookies();
     const found = cookies.find(cookie => cookie.name === '__Secure-authjs.session-token');
     sessionCookie = found.value;
+    const sessionResponse = await axios.get('https://rhombusai.com/api/auth/session', {
+        headers: { Cookie: `__Secure-authjs.session-token=${sessionCookie}` }
+    });
+    accessToken = sessionResponse.data.accessToken;
     userEmail = process.env.TEST_EMAIL;
     await browser.close();
 });
@@ -37,4 +44,32 @@ test ('negative test: unauthenticated session returns null', async () => {
     const response = await axios.get('https://rhombusai.com/api/auth/session');
     expect(response.status).toBe(200);
     expect(response.data).toBe(null); 
+});
+
+test('positive test: csv file is uploaded', async () => {
+    const form = new FormData();
+    form.append('file', fs.createReadStream('../data-validation/input.csv'));
+    form.append('title', 'input.csv');
+    form.append('description', '');
+    form.append('column_header_row', '1');
+    const response = await axios.post('https://api.rhombusai.com/api/dataset/datasets/temp/upload',form, {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            ...form.getHeaders()
+        }}
+    );
+    expect(response.status).toBe(200);
+    expect(response.data.title).toBe('input.csv');
+}); 
+
+test('negative test: file not uploaded without authentication',async() => {
+    const form = new FormData();
+    form.append('file', fs.createReadStream('../data-validation/input.csv'));
+    form.append('title', 'input.csv');
+    form.append('description', '');
+    form.append('column_header_row', '1');
+    const response = await axios.post('https://api.rhombusai.com/api/dataset/datasets/temp/upload',form);
+    expect(response.status).toBe(200); 
+    // This endpoint returns 200 without authentication.
+    //Bug: Upload should require authentication and return 401, but it returns 200. This is a security issue that needs to be fixed.
 });
